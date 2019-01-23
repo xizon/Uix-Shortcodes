fd5ea2e8a2fca818a46de30edcd122bc3cf851f2
UIUX Lab <uiuxlab@gmail.com>
2019-01-23 23:11:11 +0800

    add datepicker

--- includes/admin/uix-custom-metaboxes/init.php
+++ includes/admin/uix-custom-metaboxes/init.php
@@ -390,6 +390,11 @@ if ( !class_exists( 'Uix_Custom_Metaboxes' ) ) {
 					//Colorpicker
 					wp_enqueue_style( 'wp-color-picker' );
 					wp_enqueue_script( 'wp-color-picker' );	
+				  
+				  
+				    //date picker
+				    wp_enqueue_script('jquery-ui-datepicker');
+				  
 	
 			  }
 			
